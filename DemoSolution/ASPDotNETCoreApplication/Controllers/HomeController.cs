dd3447820c315998464a64d550b04e349b67f38a
Armen Hovsepian <rmn.hovsepian@live.com>
2019-05-21 22:46:24 +0430

    Add existing file

---
+++ DemoSolution/ASPDotNETCoreApplication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNETCoreApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Download()
+        {
+            byte[] bytes;
+            using (var ms = new MemoryStream())
+            {
+                var tw = new StreamWriter(ms);
+                tw.WriteLine("Line 1");
+                tw.WriteLine("Line 2");
+                tw.WriteLine("Line 3");
+                tw.Flush();
+                bytes = ms.ToArray();
+            }
+
+            return File(bytes, "text/plain", "YourFileName.txt");
+        }
+    }
+}
